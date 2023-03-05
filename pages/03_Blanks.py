37fd466fefa8a2eb04c77873d766da8f8dabba75
gitgeoj <58964663+gitgeoj@users.noreply.github.com>
2023-03-05 23:14:35 +0800

    Update 03_Blanks.py

--- pages/03_Blanks.py
+++ pages/03_Blanks.py
@@ -1,50 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly_express as px
-import base64
 from streamlit_extras.metric_cards import style_metric_cards
-
-# Displaying SFR Logo 
-@st.cache_data()
-def get_base64_of_bin_file(png_file):
-    with open(png_file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def build_markup_for_logo(
-    png_file,
-    background_position="50% 0%",
-    margin_top="15%",
-    image_width="60%",
-    image_height="",
-):
-    binary_string = get_base64_of_bin_file(png_file)
-    return """
-            <style>
-                [data-testid="stSidebarNav"] {
-                    background-image: url("data:image/png;base64,%s");
-                    background-repeat: no-repeat;
-                    background-position: %s;
-                    margin-top: %s;
-                    background-size: %s %s;
-                }
-            </style>
-            """ % (
-        binary_string,
-        background_position,
-        margin_top,
-        image_width,
-        image_height,
-    )
-
-def add_logo(png_file):
-    logo_markup = build_markup_for_logo(png_file)
-    st.markdown(
-        logo_markup,
-        unsafe_allow_html=True,
-    )
-
-add_logo("SFR.png")
 
 
 # Page Title and Subtext
